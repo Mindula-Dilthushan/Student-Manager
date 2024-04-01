49af7e206833f2bd79acb2f1193004e726cbd867
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2024-04-01 09:53:50 +0530

    update-user-controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+
+Route::resource('students', StudentController::class);
 
 Route::get('/', function () {
     return view('welcome');
